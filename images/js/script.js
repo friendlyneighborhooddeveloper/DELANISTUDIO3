332d36166b4ac63b5b934a8cd14f872ce4d1809d
Martin909 <>
2021-09-09 16:09:02 +0300

    makes the what we do icons into buttons

---
+++ images/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("p").toggle();
+    });
+  });
+
+
+  $(docement).ready(function(){
+  $("button").click(function(){
+    $("p").toggle();
+  });
+});
